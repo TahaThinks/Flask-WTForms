53e59259373085d05b30232b57447d6c66cc607d
TahaThinks <tahaa4587@gmail.com>
2024-06-21 18:03:11 +0400

    Add Submit Field

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email')
     password = PasswordField(label='Password')
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
